039be3e36b782f43afc51d12790b3e8807a8a47c
DanielHe09 <dandanhe09@gmail.com>
2025-09-21 23:37:01 -0400

    initial commit

---
+++ micTracker.py
@@ -0,0 +1,7 @@
+'''
+This file should:
+- have a function for tracking when your mic is muted on zoom
+- have a function for tracking when your speaking into your system mic (for now just make sure it works for your computer mic)
+- have a function for turning on your mic on zoom
+- print when your mic is muted on Zoom and you are speaking for more than 2 seconds
+'''
