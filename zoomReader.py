9f7e8b416a076c57f6fa29d324311de62b65e1a2
DanielHe09 <dandanhe09@gmail.com>
2025-09-30 14:37:22 -0400

    added function for finding the mute/unmute button in zoom

--- zoomReader.py
+++ zoomReader.py
@@ -15,6 +15,38 @@ pid=0
 for app in NSWorkspace.sharedWorkspace().runningApplications():
     if app.bundleIdentifier()=="us.zoom.xos":
         pid=app.processIdentifier()
-        break;
+        break
 
-print(pid)
+#finding the zoom AZ root and python list of all meeting windows
+if pid:
+    zoom_ax = AXUIElementCreateApplication(pid)
+    error, windows = AXUIElementCopyAttributeValue(zoom_ax, kAXWindowsAttribute, None)
+    print(error)#if value is 0 windows were successfully found
+    print(windows)
+
+'''
+desc: given a window, find if it has a mute/unmute button button using depth first search (after popping an element, we push their children to top of stack)
+arg:S
+root: window to search through
+wanted_titles: button titles to find
+'''
+def find_mute_button(root, wanted_titles={"Mute", "Unmute"}):
+    stack=[root]#initialize the stack with one element, the window's root
+    while stack:
+        element = stack.pop()
+        role = AXUIElementCopyAttributeValue(element, kAXRoleAttribute)
+        #if the current element in the window being iterated through is a button, check for button text
+        if role == kAXButtonRole:
+            title = AXUIElementCopyAttributeValue(element, kAXTitleAttribute)
+            if isinstance(title, str) and title in wanted_titles:
+                return (element, title)
+
+        #if the element's role is not a button, push it's children elements to the top of the stack
+        children = AXUIElementCopyAttributeValue(element, kAXChildrenAttribute) or []
+        stack.extend(children)
+
+    #return nothing if no mute button is found
+    return (None, None)
+
+
+#recursively iterating through the windows to find the window with the mute button
